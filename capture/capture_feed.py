3dde78392f0ed3ecd51d6894ca94fbcdc155b3c4
Owner <owner@Owners-MacBook-Pro.local>
2018-05-25 16:57:17 +0800

    image processing updates

--- capture/capture_feed.py
+++ capture/capture_feed.py
@@ -2,8 +2,14 @@ import cv2
 from mss import mss
 import numpy as np
 
-
+# captures dinosaur run game, designed for my personal computer (adjust coordinates resepctively)
 def start():
+    """
+    Captures video feed frame by frame, crops out unecessary dino and processes
+    """
+
+    sct = mss()
+
     coordinates = {
         'top': 180,
         'left': 315,
@@ -11,12 +17,22 @@ def start():
         'height': 160,
     }
 
-    sct = mss()
-
     while True:
         img = np.array(sct.grab(coordinates))
-        cv2.imshow('dino', np.array(img))
 
+        # crop out the dinosaur from the image array
+        img = img[::,75:615]
+
+        # edge detection to reduce amount of image processing work
+        img = cv2.Canny(img, threshold1=100, threshold2=200)
+
+        print(img.shape)
+        # compress the image dimensions, send it into the network
+
+        # view the network image input
+        cv2.imshow('dino', img)
+
+        # break the video feed
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
