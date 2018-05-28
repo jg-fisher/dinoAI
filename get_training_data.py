a05090231bc0327dfb1a0d1382712d3b4f04c1c1
Owner <owner@Owners-MacBook-Pro.local>
2018-05-28 16:30:35 +0800

    clean up

---
+++ get_training_data.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import selenium.webdriver.common.keys as Keys
+import time
+import threading
+from dinosaur import dinosaur
+from capture import capture_feed
+ 
+driver = webdriver.Chrome('/Users/owner/Desktop/all/tools/chromedriver')
+
+# internet connection must be off
+driver.get('http://www.google.com/')
+time.sleep(2)
+
+# main page to send key commands to
+page = driver.find_element_by_class_name('offline')
+
+# start game
+page.send_keys(u'\ue00d')
+
+# instance of dinosaur
+dino = dinosaur.Dino(driver=driver)
+
+# How I got pixel location
+#import pyautogui, sys
+#print('Press Ctrl-C to quit.')
+#try:
+#    while True:
+#        x, y = pyautogui.position()
+#        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+#        print(positionStr, end='')
+#        print('\b' * len(positionStr), end='', flush=True)
+#except KeyboardInterrupt:
+#    print('\n')
+
+# captures the dinosaur video feed
+capture_feed.start()
+
+# controls the dinosaur
+while True:
+    dino.up()
+
+
+
